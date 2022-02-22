9322db829f44a89b8e8b1c1aa660c0616f36ed94
Antonio Huerta <napoleonthevitzla@gmail.com>
2022-02-22 19:01:26 +0100

    project-started

---
+++ Pandas_modules/continents.py
@@ -0,0 +1,14 @@
+# Libraries
+import pandas as pd
+import numpy as np
+
+def new_func():
+    # Import Data
+
+#df_diamonds = pd.read_csv ('../../../../ih_datamadpt1121_project_m2/db/Diamonds_db_SQL.csv')
+
+    df_diamonds = pd.read_csv ('/Users/antoniohuerta/ironhack/ih_datamadpt1121_project_m2/db/Diamonds_db_SQL.csv')
+    df_diamonds
+
+new_func()
+
