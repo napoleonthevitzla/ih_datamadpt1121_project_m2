01adf7f18b18b25da3b31bb1420d219c35d71471
Antonio Huerta <napoleonthevitzla@gmail.com>
2022-03-01 17:26:31 +0100

    project-started

--- Pandas_modules/continents.py
+++ Pandas_modules/continents.py
@@ -2,13 +2,30 @@
 import pandas as pd
 import numpy as np
 
-def new_func():
-    # Import Data
+# Import CSV from SQL
+df_diamonds = pd.read_csv ('/Users/antoniohuerta/ironhack/ih_datamadpt1121_project_m2/db/Diamonds_db_SQL.csv')
 
-#df_diamonds = pd.read_csv ('../../../../ih_datamadpt1121_project_m2/db/Diamonds_db_SQL.csv')
+# Obtenemos el listado de valores únicos de la columna city
+cities = df_diamonds['City'].unique()
 
-    df_diamonds = pd.read_csv ('/Users/antoniohuerta/ironhack/ih_datamadpt1121_project_m2/db/Diamonds_db_SQL.csv')
-    df_diamonds
+# Creamos un diccionario al que asignamos los continentes:
 
-new_func()
+country_cont = {'Dubai':'Asia',
+                 'Kimberly':'Africa',
+                 'Las Vegas':'America',
+                 'Tel Aviv':'Asia',
+                 'Amsterdam':'Europe',
+                 'Zurich':'Europe',
+                 'Antwerp':'Europe',
+                 'Madrid':'Europe',
+                 'Paris':'Europe',
+                 'Surat':'Asia',
+                 'Luxembourg':'Europe',
+                 'London':'Europe', 
+                 'New York City':'America'
+                 }
 
+# Insertamos una columna con el continente
+df_diamonds['Continent']=df_diamonds['City'].map(country_cont)
+# Exportamos el csv
+df_diamonds.to_csv(r'/Users/antoniohuerta/ironhack/ih_datamadpt1121_project_m2/db/Diamonds_db_pandas.csv', index = False)
